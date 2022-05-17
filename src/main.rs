9282fb2b47ee99bb74c36473905c4f17513fb3bc
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-05-17 14:45:01 -0400

    add otsu

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+// use image::GenericImageView;
+use image::GrayImage;
+use imageproc::contrast::{self, threshold};
+
+// struct input {
+//     let mut image: GrayImage;
+//     let mut threshold: u8;
+// }
+
 fn main() {
-    println!("Welcome to image binarization program!");
+    let mut img = image::open("samples/image-1g.png")
+        .ok()
+        .expect("Opening image failed");
+    let img = img.crop(0, 0, img.width(), img.height());
+
+    let luma8_img: GrayImage = img.clone().to_luma8();
+    let otsu_threshold = contrast::otsu_level(&luma8_img);
+
+    let binarized_image = threshold(&luma8_img, otsu_threshold);
+    binarized_image.save("output/sample-1g.png").unwrap();
 }
